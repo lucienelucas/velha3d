da19e7dde398e89d8790b685a076f45e12834e20
lufrrj <lufrrj@gmail.com>
2014-07-07 20:11:44 +0000

    separa py do html

--- src/velha.py
+++ src/velha.py
@@ -7,11 +7,25 @@ TAM = (-1, 0, 1)
 TABULEIRO = []
 PECAS = []
 print("oi")
-pecas = [box, sphere] *9
-cores = [color.red, color.blue] *9
-TABULEIRO = [box(pos=(coluna*3, linha*3, camada*3), size=(2, 2, 2), opacity=0.2)
+pecas = [box, sphere] *14
+cores = [color.red, color.blue] *14
+class Casa:
+    def __init__(self, x, y, z):
+        self.e_casa = box(pos=(x*3, y*3, z*3), size=(2, 2, 2), opacity=0.2)
+        self.peca = None
+
+    def recebe(self, algo3d):
+        self.peca = algo3d
+        return algo3d
+
+class Peca:
+    def __init__(self, tipo_peca, x,y,z, cor):
+        self.e_peca = tipo_peca(pos=(x*3, y*3, x*3), color=cor, size=(1,1,1), opacity=0.6)
+
+TABULEIRO = [Casa(coluna, linha, camada)
              for coluna in TAM for linha in TAM for camada in TAM]
 cor = color.blue
 #peca, cor = pecas.pop()
-PECAS = [pecas.pop(pos=(coluna*3, linha*3, camada*3), color=red,  size=(1, 1, 1), opacity=0.6)
-             for coluna in TAM for linha in TAM for camada in TAM]
+PECAS = [Peca(pecas.pop(), coluna, linha, camada, cores.pop())
+              for coluna in TAM for linha in TAM for camada in TAM]
+PECASCOLOCADAS = [casa.recebe(peca) for casa, peca in zip(TABULEIRO,PECAS)]
