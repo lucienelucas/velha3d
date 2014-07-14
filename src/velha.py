f680ca4335848fc77dc24339c3c35bb5bd2f2a8e
lufrrj <lufrrj@gmail.com>
2014-07-14 20:18:53 +0000

    testando as diagonais do jogo da velha 3D

--- src/velha.py
+++ src/velha.py
@@ -1,31 +1,91 @@
 from glow import *
-_gs = glow('main')
-cena = canvas()
-cena.width = 400
-cena.height = 400
 TAM = (-1, 0, 1)
-TABULEIRO = []
-PECAS = []
+SP = 9
+SZ = 3
 print("oi")
-pecas = [box, sphere] *14
-cores = [color.red, color.blue] *14
+# equação da reta y = a*x + b
+# b = 0 então a nossa equação é y = a*x : y = -1*x a outra : y = 1*x : y = x
+# melhore o código de TIRAS para que teste mais possibilidades de ganhar
+TIRAS = [[(x*SP, y*SP, z*SP) for x in TAM] for y in TAM for z in TAM]+[
+        [(x*SP, y*SP, z*SP) for y in TAM] for x in TAM for z in TAM]+[
+    [(x*SP, y*SP, z*SP) for z in TAM] for x in TAM for y in TAM]+[
+        [(-9,9,z),(0,0,z),(0,-9,z)] for z in [-9,0,9]]+[
+        [(9,9,z),(0,0,z),(-9,-9,z)] for z in [-9,0,9]]
+
+pecas = [box, sphere] * 14
+cores = [color.red, color.blue] * 14
+
+
 class Casa:
+    CASAS = {}  # esta coleção serve para achar o objeto casa a partir de sua posicão
+
     def __init__(self, x, y, z):
-        self.e_casa = box(pos=(x*3, y*3, z*3), size=(2, 2, 2), opacity=0.2)
+        self.pos = (x*SP, y*SP, z*SP)
+        self.e_casa = box(pos=self.pos, size=(SZ, SZ, SZ), opacity=0.2)
+        Casa.CASAS[self.pos] = self  # adiciona esta casa na coleção de casas
         self.peca = None
 
     def recebe(self, algo3d):
         self.peca = algo3d
+        vencedores = self.testa_ganhou()
+        if vencedores:
+            print("ganhou")
+            self.pinta_vencedores(vencedores)
         return algo3d
 
-class Peca:
-    def __init__(self, tipo_peca, x,y,z, cor):
-        self.e_peca = tipo_peca(pos=(x*3, y*3, x*3), color=cor, size=(1,1,1), opacity=0.6)
+    def tipo_peca(self):
+        #print("tipo_peca", self.peca.tipo if self.peca is not None else 0)
+        return self.peca.tipo if self.peca else 0
 
-TABULEIRO = [Casa(coluna, linha, camada)
-             for coluna in TAM for linha in TAM for camada in TAM]
-cor = color.blue
-#peca, cor = pecas.pop()
-PECAS = [Peca(pecas.pop(), coluna, linha, camada, cores.pop())
-              for coluna in TAM for linha in TAM for camada in TAM]
-PECASCOLOCADAS = [casa.recebe(peca) for casa, peca in zip(TABULEIRO,PECAS)]
+    def clicou(self):
+        print(self.pos)
+        coluna, linha, camada = self.pos  # aposição da peça vai ser a posição da casa
+        peca = Peca(pecas.pop(), coluna, linha, camada, cores.pop())  # cria uma peça aqui
+        #Casa.CASAS.pop(self.pos)  # remove esta da lista de casas para não ser clicada
+        self.recebe(peca)  # avisa a casa que ela esta é a peça que está nela
+
+    def pinta_vencedores(self, vencedores):
+        SP = SZ+1
+        for vencedor in vencedores:
+            for posicao in vencedor:
+                print("box", posicao)
+                box(pos=posicao, color=color.yellow,  size=(SP, SP, SP), opacity=0.3)
+                # modifique para ser um box amarelo transparente do tamanho da casa
+
+    def testa_ganhou(self):
+        def casas_ganhadoras(tira):
+            tipo_tira = [Casa.CASAS[casa].tipo_peca() for casa in tira if isinstance(casa, tuple)]
+            return tipo_tira == [1, 1, 1] or tipo_tira == [2, 2, 2]
+        tiras = [tira for tira in TIRAS if casas_ganhadoras(tira)]  # crie aqui um teste para saber se alguem venceu
+        #print("testa_ganhou", tiras,  casas_ganhadoras(tiras))
+        return tiras
+
+
+class Peca:
+    def __init__(self, tipo_peca, x, y, z, cor):
+        self.e_peca = tipo_peca(pos=(x, y, z), color=cor,  size=(SZ, SZ, SZ), opacity=0.6)
+        self.tipo = 1 if tipo_peca == box else 2
+
+
+def main():
+
+    def clicou(event):
+        cc = cena.mouse.pick().pos  # pega a posição do objeto clicado pelo mouse
+        casa_clicada = (cc.x, cc.y, cc.z)  # cria uma tripla ordenada no espaço
+        if casa_clicada in Casa.CASAS.keys():  # procura a tripla na coleção de casas
+            casa_clicada = Casa.CASAS[casa_clicada]
+            if casa_clicada in TABULEIRO:
+                TABULEIRO.remove(casa_clicada)
+                casa_clicada.clicou()  # chama o clicou da casa escolhida
+    _gs = glow('main')
+    cena = canvas()
+    cena.width = 400
+    cena.height = 400
+    cena.bind("mousedown", clicou)
+
+    TABULEIRO = [Casa(coluna, linha, camada)
+                 for coluna in TAM for linha in TAM for camada in TAM]
+    return TABULEIRO
+
+if __name__ == "__main__":
+    main()
